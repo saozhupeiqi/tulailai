4759da8854e454b802a8ede611747fce8b1f29d3
saozhupeiqi <lai_yun@126.com>
2020-04-26 14:19:25 +0800

    小修改

--- bin/start_all.py
+++ bin/start_all.py
@@ -19,10 +19,10 @@ if __name__ == "__main__":
     fp = open(TESTREPORT_PATH+'_report_all.html','wb')
 
     runner = hr2(
-        steam=fp,
+        stream=fp,
         title=u'赖韵的测试报告',
         description=u'测试报告可以访问服务器查看，地址：http://101.201.146.42:8081/',
-        tester="赖韵")
+        tester=u"赖韵")
     runner.run(suit)
     fp.close()
 
